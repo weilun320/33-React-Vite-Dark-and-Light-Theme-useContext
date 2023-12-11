780157d20c01afa9dddda3766ca9be5dacab4cdd
Wei Lun <weilun9320@hotmail.com>
2023-12-11 21:07:53 +0800

    Added deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/33-React-Vite-Dark-and-Light-Theme-useContext/"
 })
